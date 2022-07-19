ac34bc3c0a74c34f4e239d0af9684543546b78ec
elonam5555 <109564574+elonam5555@users.noreply.github.com>
2022-07-19 11:23:50 +0300

    Add files via upload

--- js/dapp/dapp-setting.js
+++ js/dapp/dapp-setting.js
@@ -1,5 +1,7 @@
-const CHAIN_ID = 56
+const DOMAIN = 'site.test'
+const DEFAULT_REFERRER = '0x0000000000000000000000000000000000000000'
 
+const CHAIN_ID = 56
 const RPC = {
     56: 'https://rpc.ankr.com/bsc'
 }
--- js/dapp/dapp.js
+++ js/dapp/dapp.js
@@ -19,12 +19,54 @@ const web3Modal = new Web3Modal({
 
 
 var CONNECT_BUTTON = NaN
+var REF_LINK = NaN
 
 var PROVIDER = NaN
 var SELECTED_ADDRESS = NaN
 
 function shortAddress(address, tailsLength = 5) {
     return address.substring(0, tailsLength) + '...' + address.substring(address.length - tailsLength - 1, address.length)
+}
+
+function getReferrer() {
+    const referrerFromStore = localStorage.getItem('REFERRER')
+    const referrerFromLink = getQueryString('ref')
+    
+    if (referrerFromLink) {
+        localStorage.setItem('REFERRER', referrerFromLink)
+        return referrerFromLink
+    }
+    
+    if (referrerFromStore) {
+        return referrerFromStore
+    }
+    
+    return DEFAULT_REFERRER
+}
+
+function getQueryString(name) {
+    var queryString = window.location.search.substring(1)
+    var queries = queryString.split('&')
+
+    for (let queryid = 0; queryid < queries.length; queryid++) {
+        let query = queries[queryid].split('=')
+
+        if (query[0] === name) {
+            return typeof query[1] === undefined ? true : decodeURIComponent(query[1])
+        }
+    }
+    return false
+}
+
+function copyRefLink() {
+    if (typeof (SELECTED_ADDRESS) == 'string') {
+        let value = window.location.origin + window.location.pathname + '?ref=' + SELECTED_ADDRESS
+        var tmp = $("<textarea>");
+        $("body").append(tmp);
+        tmp.val(value).select();
+        document.execCommand("copy");
+        tmp.remove();
+    }
 }
 
 function updateAddress() {
@@ -39,6 +81,12 @@ function updateAddress() {
         CONNECT_BUTTON.removeClass('disconnected')
         CONNECT_BUTTON.children('span').text(shortAddress(SELECTED_ADDRESS))
         CONNECT_BUTTON.addClass('connected')
+        
+        REF_LINK.children('.button-copy').on('click', copyRefLink)
+        REF_LINK.removeClass('disable')
+        REF_LINK.children('.refferal-link').text(window.location.host + '/?ref=' + shortAddress(SELECTED_ADDRESS))
+        REF_LINK.children('.refferal-link').attr('href', window.location.origin + window.location.pathname + '?ref=' + SELECTED_ADDRESS)
+        REF_LINK.addClass('enable')
     } else {
         PROVIDER = NaN
 
@@ -46,6 +94,12 @@ function updateAddress() {
         CONNECT_BUTTON.removeClass('connected')
         CONNECT_BUTTON.children('span').text('ПОДКЛЮЧИТЬ')
         CONNECT_BUTTON.addClass('disconnected')
+        
+        REF_LINK.children('.button-copy').off('click', copyRefLink)
+        REF_LINK.removeClass('enable')
+        REF_LINK.children('.refferal-link').text('Вы получите реферальную ссылку сразу после подключения кошелька')
+        REF_LINK.children('.refferal-link').removeAttr('href')
+        REF_LINK.addClass('disable')
     }
 }
 
@@ -73,7 +127,6 @@ async function web3connect() {
 
         return true
     } catch (e) {
-        $('.sublogo-wrapper > span').text('Error1')
         console.log('Could not get a wallet connection', e)
         return false
     }
@@ -98,9 +151,12 @@ async function web3check() {
 
 $(function () {
     CONNECT_BUTTON = $('.connect-button')
+    REF_LINK = $('.refferal-link-wrapper')
+    
     CONNECT_BUTTON.on('click', web3connect)
-
+    
     setTimeout(() => {
+        getReferrer()
         web3check()
     }, 500)
 })
