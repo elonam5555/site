aa8c2892c46b31d27610e2a0bba916037f490454
elonam5555 <109564574+elonam5555@users.noreply.github.com>
2022-07-21 15:43:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,12 +13,26 @@ function scrollToBottom() {
     })
 }
 
+function bodyMove() {
+    $('.body').css("top", "15px")
+    $('.body').css("top", "-15px")
+    $('.body').css("top", "0px")
+}
+
+async function connectBeacon() {
+    setInterval(function () {
+        $('.connect-button.disconnected').toggleClass("hover");
+    }, 500)
+}
+
 $(function () {
-    scrollToTop()
     $('.contacts-link').on('click', scrollToBottom)
 })
 
 $(document).ready(function() {
+    connectBeacon()
+    bodyMove()
+    scrollToTop()
     $('.contacts-section li>div>button').tooltipster({
         theme: 'tooltipster-noir'
     });
